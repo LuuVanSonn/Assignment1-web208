14647f7dbfff3f96e0636a51d7b61b0918a7ff5e
LuuVanSonn <sonlvph20306@fpt.edu.vn>
2023-05-28 09:54:32 +0700

    base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServicesComponent } from './services/services.component';
+import { HomeComponent } from './home/home.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { BaseLayoutComponent } from './layouts/base-layout/base-layout.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import { ProductPageComponent } from './pages/product-page/product-page.component';
+import { AdminProductComponent } from './pages/admin/admin-product/admin-product.component';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { ProductAddComponent } from './pages/admin/product-add/product-add.component';
+import { ProductEditComponent } from './pages/admin/product-edit/product-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServicesComponent,
+    HomeComponent,
+    ProductListComponent,
+    AdminLayoutComponent,
+    BaseLayoutComponent,
+    AboutPageComponent,
+    AdminComponent,
+    HomePageComponent,
+    PageNotFoundComponent,
+    ProductDetailComponent,
+    ProductPageComponent,
+    AdminProductComponent,
+    DashboardComponent,
+    ProductAddComponent,
+    ProductEditComponent
   ],
   imports: [
     BrowserModule,
